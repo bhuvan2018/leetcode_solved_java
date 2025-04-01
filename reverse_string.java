99b43cb87d3ceb5c05770c2c6dd512dd7c2dab5d
Bhuvan_shetty <bhuvanshetty2018@gmail.com>
2025-04-01 12:29:28 +0530

    commit

---
+++ reverse_string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void reverseString(char[] s) {
+        char[] res =new char[s.length];
+        for (int i=0;i<s.length;i++) {
+            res[s.length-1-i]=s[i];
+        }
+        for(int i=0;i<s.length;i++){
+            s[i]=res[i];
+        }
+    }
+}
