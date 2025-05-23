777667cf6d462611f04695f847036f0227eca11e
bhuvan2018 <bhuvanshetty2018@gmail.com>
2025-05-23 19:37:22 +0530

    Add contains_duplicate_hashmap.java to implement algorithm for checking duplicates in an array

---
+++ contains_duplicate_hashmap.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate_hashmap(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int key:map.keySet()){
+            if(map.get(key)>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
