e8ee89a6853d2a46386503a0e2bdfae024569ef5
bhuvan2018 <bhuvanshetty2018@gmail.com>
2025-05-26 23:25:58 +0530

    Refactor formatting in reverseString method for improved readability

--- reverse_string.java
+++ reverse_string.java
@@ -1,7 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         char[] res =new char[s.length];
-        for (int i=0;i<s.length;i++) {
+        for(int i=0;i<s.length;i++) {
             res[s.length-1-i]=s[i];
         }
         for(int i=0;i<s.length;i++){
